59a4686ebbb6de586aacf68e5b7402677a88027f
abhishek <abhitiwari472@gmail.com>
2017-09-01 19:25:11 +0530

    working on project euler 4

--- ProjectEuler/projectE_3.py
+++ ProjectEuler/projectE_3.py
@@ -2,15 +2,15 @@
 #
 # What is the largest prime factor of the number 600851475143 ?
 import math
-def largest_prime(i):
+def largest_prime(i):   # first removing all 2 that are the factors
     lists = []
     while(i%2 == 0):
         i/=2
 
-    for x in range(3,int(math.sqrt(i))):
+    for x in range(3,int(math.sqrt(i))):  # starting from 3 to the sqrt of the number we are checking if divisible
         if(i % x == 0):
             lists.append(x)
-            i/=x
+            i/=x                          # removing a divisor upon iteration so successive iterations take less time
 
 
     return lists
--- ProjectEuler/projectE_4.py
+++ ProjectEuler/projectE_4.py
@@ -2,3 +2,76 @@
 #  numbers is 9009 = 91 × 99.
 #
 # Find the largest palindrome made from the product of two 3-digit numbers.
+import time
+
+def isPalin(x):
+    if (type(x) != str):
+        x = str(x)
+    list_num = list(x)
+    if (len(list_num) != 1):
+        while (len(list_num) > 0):
+            if (list_num[0] == list_num[-1]):
+                if (len(list_num) != 1):
+                    list_num.remove(list_num[0])
+                    list_num.remove(list_num[-1])
+                else:
+                    break
+            else:
+                return False
+    else:
+        return "single digit numbers can't be palindromes"
+    return True
+
+def isPalin2(num):
+    if str(num)==str(num)[::-1]: return True
+    else: return False
+
+
+def isPalin3(num):
+    reversed = 0
+    original = num
+
+    if num < 10: return True
+    if num % 10 == 0: return False
+
+    while num >= 1:
+        reversed = (reversed * 10) + (num % 10)
+        num = num / 10
+
+    if original == reversed:
+        return True
+    else:
+        return False
+
+
+start = time.time()
+for i in range(10000000, 10000002): # my palindrome logic is more efficient
+    a =isPalin(i)
+elapsed = time.time() - start
+
+print(elapsed)
+
+
+start = time.time()
+for i in range(10000000, 10000002): # my palindrome logic is more efficient in case of larger numbers
+    a =isPalin2(i)
+elapsed = time.time() - start
+
+print(elapsed)
+
+# start = time.time()
+# for i in range(1000, 1000000): # my palindrome logic is more efficient
+#     a =isPalin3(i)
+# elapsed = time.time() - start
+
+# print(elapsed)
+
+
+
+
+
+
+
+
+
+
