873be30cb27b3c2c0d913cca6dde765685db3666
abhishek <abhitiwari472@gmail.com>
2017-10-23 13:22:15 +0530

    committing beyond basic function part

--- Python2Lambdas/lambds.py
+++ Python2Lambdas/lambds.py
@@ -3,3 +3,6 @@ scientist = ['mariecurie', 'pierrecurie', 'issacasimov', 'nikoltesla', 'johnnash
 
 
 print(sorted(scientist, key=lambda name: name.split()[-1]))
+
+key = lambda name : name.split()[-1]
+print(key("nikola tesla"))
